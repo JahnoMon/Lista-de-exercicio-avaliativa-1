d61eea001dc05b8ebee079cdc6330df0e4a14644
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-08 23:26:19 -0300

    Update Questao-1.java

--- Questao-1.java
+++ Questao-1.java
@@ -1,3 +1,9 @@
 /*
+A linguagem de programação Java é conhecida, dentre outras características,
+por sua portabilidade. Explique, através de cerca de dois parágrafos, que elementos da
+linguagem permitem essa portabilidade.
 
+- O que permite a portabilidade do Java é o Java Virtual Machine (JVM), que independe 
+do sistema operacional para executar os códigos java pois a JVM lê o bytecode e o 
+adapta para o ambiente em que está sendo executado no momento.
 */
