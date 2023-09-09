dbaffa5901777d31c05a9999a01942ff7ebd63e8
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-08 23:53:37 -0300

    Create Questao-7.java

---
+++ Questao-7.java
@@ -0,0 +1,8 @@
+/*
+Escreva em Java classes referentes a peças de xadrez. Cada peça de xadrez
+tem um valor referente a ela, quando são capturadas, assim como cada peça de xadrez
+pode, no contexto, ocupar uma posição do tabuleiro (indicada por seu x e y). Escreva o
+método possiveisMovimentos(), que, considerando a posição atual da peça, indica quais
+são os possíveis movimentos dela. Esse método deve funcionar de forma diferente para
+cada peça. Utilize dos princípios de orientação a objetos para fazer isso da melhor maneira.
+*/
