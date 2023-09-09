1c43e6199cf26d7b821861f2cbbe1a0f4527ce03
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-08 23:56:38 -0300

    Create Questao-10.java

---
+++ Questao-10.java
@@ -0,0 +1,9 @@
+/*
+Crie uma classe Java que representa figuras geométricas genéricas. Forneça
+uma maneira estática de sabermos quantas figuras geométricas foram instanciadas ao
+longo do programa. Faça com que essa classe seja a base para figuras geométricas mais
+específicas. Para cada figura geométrica, forneça uma maneira de calcular a sua área e
+obter seu número de lados. Por fim, crie um programa que contenha uma coleção dessas
+figuras e, através de um laço for-each, imprima a área de todas as figuras contidas nesta
+coleção.
+*/
