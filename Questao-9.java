c09f576050374c2e7a01d738b6659d1ee0b205b7
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-08 23:55:39 -0300

    Create Questao-9.java

---
+++ Questao-9.java
@@ -0,0 +1,5 @@
+/*
+Crie a classe Stack, que represente uma pilha que armazena strings.
+Implemente métodos de adição e remoção de elementos, verificação de se a pilha está
+vazia e construtores.
+*/
