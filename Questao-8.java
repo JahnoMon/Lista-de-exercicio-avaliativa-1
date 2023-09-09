d0ee7b566c86bf9dff3b9fbed18b81ed43cde784
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-08 23:54:39 -0300

    Create Questao-8.java

---
+++ Questao-8.java
@@ -0,0 +1,10 @@
+/*
+Escreva uma classe referente a Data, que contém dia, mês e ano. As datas
+podem ser criadas de duas maneiras: recebendo o dia, mês e ano correspondentes a ela ou
+nenhum valor, caso esse em que se entende que o dia, mês e ano dessa data é o dia, mês
+e ano atuais. Implemente alguns métodos nessa classe, listados abaixo:
+
+a) diaDaSemana(): retorna uma String que diz qual é o dia da semana da Data
+b) diasDeDiferenca(Data outra), retorna um inteiro indicando o número de dias de diferença entre o objeto data a partir do qual o método foi chamado e uma outra data 
+passada como argumento.
+*/
