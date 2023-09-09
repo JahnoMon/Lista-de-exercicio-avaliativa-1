89d6759a8023105f753a1b17ae5857cc49b42395
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-08 23:53:00 -0300

    Create Questao-6.java

---
+++ Questao-6.java
@@ -0,0 +1,8 @@
+/*
+Desenvolva em Java uma classe que representa o arcabouço lógico do
+jogo-da-velha. Nele, as seguintes funcionalidades devem existir:
+
+a) jogar(int x, int y, Simbolo s): joga s na posição x, y do tabuleiro.
+b) checar(): verifica o estado de jogo, retornando se 1 se o jogador de ‘O’ venceu, -1 seo jogador de ‘X’ venceu e 0 se ninguém venceu.
+c) limpar(): reseta o tabuleiro.
+*/
