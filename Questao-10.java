265bdf041dab58c729e9699c3df894326fc76bca
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-14 18:30:44 -0300

    Update Questao-10.java

--- Questao-10.java
+++ Questao-10.java
@@ -7,3 +7,82 @@ obter seu número de lados. Por fim, crie um programa que contenha uma coleção
 figuras e, através de um laço for-each, imprima a área de todas as figuras contidas nesta
 coleção.
 */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FiguraGeometrica{
+    private static int contador = 0;
+
+    public FiguraGeometrica(){
+        contador++;
+    }
+
+    public double calcularArea(){
+
+        return 0.0;
+    }
+
+    public int getNumeroDeLados(){
+        return 0;
+    }
+
+    public static int getNumeroDeFigurasCriadas(){
+        return contador;
+    }
+}
+
+class Retangulo extends FiguraGeometrica{
+    private double altura;
+    private double largura;
+
+    public Retangulo(double altura, double largura){
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    @Override
+    public double calcularArea(){
+        return altura * largura;
+    }
+
+    @Override
+    public int getNumeroDeLados(){
+        return 4;
+    }
+}
+
+class Triangulo extends FiguraGeometrica{
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura){
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea(){
+        return (base * altura) / 2;
+    }
+
+    @Override
+    public int getNumeroDeLados(){
+        return 3;
+    }
+}
+
+public class ProgramaFiguras{
+    public static void main(String[] args){
+        List<FiguraGeometrica> figuras = new ArrayList<>();
+        figuras.add(new Retangulo(5.0, 3.0));
+        figuras.add(new Triangulo(4.0, 6.0));
+
+        for (FiguraGeometrica figura : figuras){
+            System.out.println("Área: " + figura.calcularArea());
+            System.out.println("Número de Lados: " + figura.getNumeroDeLados());
+        }
+
+        System.out.println("Total de figuras criadas: " + FiguraGeometrica.getNumeroDeFigurasCriadas());
+    }
+}
