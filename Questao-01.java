41bddcf30603c803a1c684bb0bde27254addf6ef
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-25 11:11:24 -0300

    Update Questao-01.java

--- Questao-01.java
+++ Questao-01.java
@@ -3,7 +3,21 @@ A linguagem de programação Java é conhecida, dentre outras características,
 por sua portabilidade. Explique, através de cerca de dois parágrafos, que elementos da
 linguagem permitem essa portabilidade.
 
-- O que permite a portabilidade do Java é o Java Virtual Machine (JVM), que independe 
-do sistema operacional para executar os códigos java pois a JVM lê o bytecode e o 
-adapta para o ambiente em que está sendo executado no momento.
+- A portabilidade da linguagem de programação Java é uma de suas características distintivas, 
+e isso se deve principalmente ao uso da Máquina Virtual Java (JVM) e ao conceito "Write Once, 
+Run Anywhere" (Escreva uma vez, execute em qualquer lugar). O Java atinge essa portabilidade 
+graças à compilação de código-fonte em bytecode intermediário em vez de código de máquina nativo. 
+Esse bytecode é executado na JVM, que é específica para cada plataforma, permitindo que o mesmo 
+ódigo Java seja executado em diferentes sistemas operacionais e arquiteturas de hardware, desde 
+que haja uma JVM disponível para essa plataforma. Isso significa que os desenvolvedores podem 
+escreveraplicativos Java em uma máquina e, em seguida, executá-los em várias plataformas sem a 
+necessidade de reescrever o código-fonte para cada uma delas. Isso é fundamental para a abordagem 
+"compile once, run anywhere" do Java, tornando-o uma escolha popular para o desenvolvimento de 
+aplicativos multiplataforma.
+
+Além disso, o uso de uma biblioteca padrão rica e consistente, como as APIs da plataforma Java, 
+garante que as funcionalidades essenciais estejam disponíveis independentemente da plataforma, 
+tornando mais fácil para os desenvolvedores criar aplicativos que funcionem de maneira consistente 
+em diferentes ambientes. Isso, juntamente com a natureza independente de plataforma do bytecode 
+Java, contribui significativamente para a portabilidade da linguagem..
 */
