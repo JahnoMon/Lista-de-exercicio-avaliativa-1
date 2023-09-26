f88a5e60102ab588855059dc1b7a69d405b6263b
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-09-26 18:32:53 -0300

    50%

--- Questao-07.java
+++ Questao-07.java
@@ -90,19 +90,24 @@ class Rei extends PecaXadrez{
     }
 }
 
-class Rainha extends PecaXadrez{
-    public Rainha(int x, int y){
+class Rainha extends PecaXadrez {
+    public Rainha(int x, int y) {
         super(x, y, 10);
     }
 
     @Override
-    public void possiveisMovimentos(){
-        System.out.println("Possiveis movimentos para o Rei na posiaoo (" + x + ", " + y + "):");
+    public void possiveisMovimentos() {
+        System.out.println("Possiveis movimentos para a Rainha na posicao (" + x + ", " + y + "):");
         System.out.println("");
-        for (int i = 0; i <= 7; i++){
-            for (int j = 0; j <= 7; j++){
-                if (i >= 0 && i <= 7 && j >= 0 && j <= 7 && ((i < x - 1 && j  == y - 1) || (i > x - 1 && j  == y - 1) || (i == x - 1 && j < y - 1) || (i == x - 1 && j > y - 1) || (j == i)) && !(i == x-1 && j == y-1)) {
-                    System.out.print("P ");
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                if (i != x || j != y) {
+                    // so assim p diagonal inversa:
+                    if (i == x || j == y || Math.abs(i - x) == Math.abs(j - y)) { 
+                        System.out.print("P ");
+                    } else {
+                        System.out.print("- ");
+                    }
                 } else {
                     System.out.print("- ");
                 }
@@ -181,8 +186,29 @@ class Cavalo extends PecaXadrez{
 
 class Main{
     public static void main(String[] args){
-        Peao rei = new Peao(3, 3);
+        Rei rei = new Rei(3, 3);
         rei.possiveisMovimentos();
+        System.out.print("");
+        
+        Peao peao = new Peao(3, 3);
+        peao.possiveisMovimentos();
+        System.out.print("");
+        
+        Rainha rainha = new Rainha(3, 3);
+        rainha.possiveisMovimentos();
+        System.out.print("");
+
+        Torre torre = new Torre(3, 3);
+        torre.possiveisMovimentos();
+        System.out.print("");
+
+        Bispo bispo = new Bispo(3, 3);
+        bispo.possiveisMovimentos();
+        System.out.print("");
+
+        Cavalo cavalo = new Cavalo(3, 3);
+        cavalo.possiveisMovimentos();
+        System.out.print("");
     }
 }
 
